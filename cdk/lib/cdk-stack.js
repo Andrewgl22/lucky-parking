9820576880ea2fc494e6e7f6e0abd4c8d324d90f
Andrewgl22 <ledermanmusic@gmail.com>
2023-08-17 15:36:53 -0700

    added combined dockerfile

--- cdk/lib/cdk-stack.js
+++ cdk/lib/cdk-stack.js
@@ -1,23 +1,35 @@
-const { Stack, Duration } = require('aws-cdk-lib');
-// const sqs = require('aws-cdk-lib/aws-sqs');
+const cdk = require('@aws-cdk/core');
+const eb = require('@aws-cdk/aws-elasticbeanstalk');
+const assets = require('@aws-cdk/aws-s3-assets');
 
-class CdkStack extends Stack {
-  /**
-   *
-   * @param {Construct} scope
-   * @param {string} id
-   * @param {StackProps=} props
-   */
-  constructor(scope, id, props) {
-    super(scope, id, props);
+class MyEbStack extends cdk.Stack {
+    constructor(scope, id, props) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // Docker image for the server
+        const serverDockerAsset = new assets.DockerImageAsset(this, 'ServerDockerImage', {
+            directory: './server', 
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: Duration.seconds(300)
-    // });
-  }
+        // Docker image for React
+        const reactDockerAsset = new assets.DockerImageAsset(this, 'ReactDockerImage', {
+            directory: './client',
+        });
+
+        // EB environment for the server
+        const serverEbEnv = new eb.CfnEnvironment(this, 'ServerEnvironment', {
+            applicationName: 'YourServerAppName',
+            solutionStackName: '64bit Amazon Linux 2 v3.4.4 running Docker',
+            versionLabel: serverDockerAsset.imageUri,
+        });
+
+        // EB environment for React
+        const reactEbEnv = new eb.CfnEnvironment(this, 'ReactEnvironment', {
+            applicationName: 'YourReactAppName',
+            solutionStackName: '64bit Amazon Linux 2 v3.4.4 running Docker',
+            versionLabel: reactDockerAsset.imageUri,
+        });
+    }
 }
 
-module.exports = { CdkStack }
+module.exports = { MyEbStack };
---
+++ dockerfile
@@ -0,0 +1,29 @@
+# ---- Build React App ----
+FROM node:alpine AS react-build
+WORKDIR /app
+COPY ./client/package*.json ./  
+RUN npm install
+COPY ./client ./
+RUN npm run build
+
+# ---- Build Server App ----
+FROM node:alpine AS server-build
+WORKDIR /app
+COPY ./server/package*.json ./
+RUN npm install
+COPY ./server ./
+
+# ---- Nginx Setup ----
+FROM nginx:alpine
+COPY --from=react-build /app/build /usr/share/nginx/html
+
+# Copy Nginx default.conf with configurations for reverse proxy (assuming you have one)
+COPY ./nginx/default.conf /etc/nginx/conf.d/
+
+# Copy server node modules and other files
+WORKDIR /app
+COPY --from=server-build /app .
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]

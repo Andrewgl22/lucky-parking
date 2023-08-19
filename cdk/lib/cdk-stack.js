4a1d5be9b809c6ec062f95ccb5d4b5079186e7cd
Andrewgl22 <ledermanmusic@gmail.com>
2023-08-19 10:24:18 -0700

    1st test of ci/cd

--- cdk/lib/cdk-stack.js
+++ cdk/lib/cdk-stack.js
@@ -6,28 +6,16 @@ class MyEbStack extends cdk.Stack {
     constructor(scope, id, props) {
         super(scope, id, props);
 
-        // Docker image for the server
-        const serverDockerAsset = new assets.DockerImageAsset(this, 'ServerDockerImage', {
-            directory: './server', 
+        // Docker image that encapsulates both frontend and server
+        const unifiedDockerAsset = new assets.DockerImageAsset(this, 'UnifiedDockerImage', {
+            directory: '../../', // this directory should contain a Dockerfile that sets up both frontend and server
         });
 
-        // Docker image for React
-        const reactDockerAsset = new assets.DockerImageAsset(this, 'ReactDockerImage', {
-            directory: './client',
-        });
-
-        // EB environment for the server
-        const serverEbEnv = new eb.CfnEnvironment(this, 'ServerEnvironment', {
-            applicationName: 'YourServerAppName',
+        // EB environment for the unified application
+        const unifiedEbEnv = new eb.CfnEnvironment(this, 'UnifiedEnvironment', {
+            applicationName: 'LP-EB-app',
             solutionStackName: '64bit Amazon Linux 2 v3.4.4 running Docker',
-            versionLabel: serverDockerAsset.imageUri,
-        });
-
-        // EB environment for React
-        const reactEbEnv = new eb.CfnEnvironment(this, 'ReactEnvironment', {
-            applicationName: 'YourReactAppName',
-            solutionStackName: '64bit Amazon Linux 2 v3.4.4 running Docker',
-            versionLabel: reactDockerAsset.imageUri,
+            versionLabel: unifiedDockerAsset.imageUri,
         });
     }
 }

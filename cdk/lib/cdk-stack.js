0530fa9addbd1693fc3df5cda19c9a0730c6c74f
Andrewgl22 <ledermanmusic@gmail.com>
2023-08-22 15:51:20 -0700

    trying again

--- cdk/lib/cdk-stack.js
+++ cdk/lib/cdk-stack.js
@@ -1,39 +1,34 @@
 const cdk = require('aws-cdk-lib');
 const eb = require('aws-cdk-lib/aws-elasticbeanstalk');
+const ecr = require('aws-cdk-lib/aws-ecr');
 const assets = require('aws-cdk-lib/aws-ecr-assets');
-const path = require('path');
 
 class MyEbStack extends cdk.Stack {
-    constructor(scope, id, props) {
-        super(scope, id, props);
+  constructor(scope, id, props) {
+    super(scope, id, props);
 
-        // Docker image that encapsulates both frontend and server
-        const unifiedDockerAsset = new assets.DockerImageAsset(this, 'UnifiedDockerImage', {
-            directory: '../../lucky-parking',
-        });
+    // Docker image asset
+    const dockerAsset = new assets.DockerImageAsset(this, 'MyDockerImageAsset', {
+      directory: '../../lucky-parking',
+    });
 
-        // Create Elastic Beanstalk Application
-        const ebApp = new eb.CfnApplication(this, 'MyEBApplication', {
-            applicationName: 'LP-EB-app'
-        });
+    // Elastic Beanstalk application
+    const ebApp = new eb.CfnApplication(this, 'MyEBApplication', {
+      applicationName: 'MyEBApp',
+    });
 
-        const ebAppVersion = new eb.CfnApplicationVersion(this, 'AppVersion', {
-            applicationName: ebApp.applicationName,
-            sourceBundle: {
-                s3Bucket: unifiedDockerAsset.s3BucketName,
-                s3Key: unifiedDockerAsset.s3ObjectKey
-            },
-            versionLabel: '1.0.0',
-        });
-
-        // Create Elastic Beanstalk Environment
-        const unifiedEbEnv = new eb.CfnEnvironment(this, 'UnifiedEnvironment', {
-            applicationName: ebApp.applicationName,
-            dependsOn: [ebApp, ebAppVersion],
-            solutionStackName: '64bit Amazon Linux 2 v3.6.0 running Docker',
-            versionLabel: '1.0.0',
-        });
-    }
+    const ebEnv = new eb.CfnEnvironment(this, 'MyEBEnvironment', {
+        applicationName: ebApp.applicationName,
+        solutionStackName: '64bit Amazon Linux 2 v3.6.0 running Docker',
+        optionSettings: [
+          {
+            namespace: 'aws:elasticbeanstalk:application:environment',
+            optionName: 'DOCKER_IMAGE_URI',
+            value: dockerAsset.imageUri,
+          },
+        ],
+      });
+  }
 }
 
 module.exports = { MyEbStack };
